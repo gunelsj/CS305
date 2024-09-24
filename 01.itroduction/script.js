1aae4da45a6efccc194453ddb053475ebaf408a1
gunelsj <gunelsj@code.edu.az>
2024-09-24 14:05:37 +0400

    Task1

---
+++ 01.itroduction/script.js
@@ -0,0 +1,23 @@
+
+const username ="Gunel"
+const surname = "Jafarova"
+let position = "Cyber Security Specialist"
+let city = "Baku"
+let country = "Azerbaijan" 
+const university = "Stanford"
+let age = "20"
+
+console.log(username);
+console.log(age)
+console.log(surname)
+console.log(city)
+ console.log(country)
+ console.log(univerity)
+ console.log(position)
+console.log(`Name ${Gunel} Surname ${Jafarova}`) 
+let isAdmin = Gunel 
+
+
+
+
+
