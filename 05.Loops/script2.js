a82c92dfb040325abbe8ec37a7ffd89f51e6867a
gunelsj <gunelsj@code.edu.az>
2024-09-25 22:53:49 +0400

    Changes

---
+++ 05.Loops/script2.js
@@ -0,0 +1,28 @@
+let text = "Code Academy"
+let city = 'Baku'
+let country = "Azerbaijan"
+
+for (let i = 0; i < text.length; i++) {
+    console.log(text[i])
+    }
+console.log(city.concat(" ", country))
+
+console.log(text.split(''))
+
+console.log(text.split( " " ))
+  
+if (text.includes('CS')) {
+    console.log( "S")
+} else {
+    console.log('X') }
+
+
+    for (let i = 0; i <= 20; i++) {
+        console.log(i)
+    }
+
+    for (x of country)
+    {
+        console.log(x);
+        
+    }
---
+++ classwork/script.js
@@ -0,0 +1,60 @@
+
+const username ="Gunel"
+const surname = "Jafarova"
+let position = "Cyber Security Specialist"
+let city = "Baku"
+let country = "Azerbaijan" 
+const university = "Stanford"
+let age = "20"
+
+console.log(username);
+console.log(age)
+console.log(surname)
+console.log(city)
+ console.log(country)
+ console.log(university)
+ console.log(position)
+console.log(`Name ${Gunel} Surname ${Jafarova}`) 
+let isAdmin = Gunel 
+
+
+switch(num) {
+    case 66:
+        case 14: 
+        console.log("result 1");
+        break
+        case 6:
+        console.log("result 2");
+        case 9:
+        case 8:
+            console.log("result 3");
+            break
+            case 14:
+                console.log("result 4");     
+        break
+        default:
+        console.log("end");
+    }
+
+
+        if  (num === 66 || num === 14) {
+            console.log("result 1");
+        } else if ( num === 6) {
+            console.log("result 2");
+        } else if ( num === 9 || num === 8) {
+            console.log("result 3");
+         } else if ( num === 14){
+             console.log("result 4");     
+         } else {
+            console.log("end"); 
+        }
+
+
+        let user = "gunel"
+        let result = ( user[0].toUpperCase()+ user.slice(1, 4) ) 
+        console.log(result) 
+        
+        
+
+
+
